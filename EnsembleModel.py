174c17a0353f62bb1b27550ab71548828af1a68b
FabioUMa2 <149825824+FabioUMa2@users.noreply.github.com>
2024-04-26 13:19:36 +0100

    Update EnsembleModel
    Was implemented the Voting Classifier and Gradient Boosting Classifier

--- EnsembleModel.py
+++ EnsembleModel.py
@@ -1,3 +1,10 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier, GradientBoostingClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import pandas as pd
+
 class EnsembleModel:
 
     def __init__(self, data_loader):
@@ -6,3 +13,45 @@ class EnsembleModel:
         self._labels_train = self.data_loader.labels_train
         self._data_test = self.data_loader.data_test.select_dtypes(include=['number'])
         self._labels_test = self.data_loader.labels_test
+
+
+    def VotingClassifier(self):
+        lr = LogisticRegression(random_state=1)
+        kn = KNeighborsClassifier(n_neighbors=5)
+        svc = SVC(kernel='rbf', probability=True)
+
+        eclf = VotingClassifier(estimators = [('lr', lr), ('kn', kn), ('svc', svc)],voting = 'hard')
+
+
+        classifiers = [('Logistic Regression', lr), ('KNeighborsClassifier', kn), ('SVC', svc), ('Ensemble', eclf)]
+        for label, clf in classifiers:
+            scores = cross_val_score(clf, self._data_train, self._labels_train,
+                                     scoring='accuracy', cv=5)
+            print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+
+
+    def GradientBoostingClassifier(self):
+
+        gb = GradientBoostingClassifier(random_state=0)
+
+        param_grid = dict (
+            n_estimators = [5,7,9],
+            learning_rate = [0.05, 0.1, 0.2],
+            max_depth = [1, 3]
+        )
+
+        print("Numero de Combinações:", len(param_grid['n_estimators']) * len(param_grid['learning_rate']) * len(param_grid['max_depth']))
+
+        grid_search = GridSearchCV(gb, param_grid=param_grid, scoring='roc_auc', cv=3)
+
+        grid_search.fit(self._data_train, self._labels_train)
+
+        print("Best Parameters Configuration: ", grid_search.best_params_)
+        # Best Parameters Configuration:  {'learning_rate': 0.05, 'max_depth': 1, 'n_estimators': 5}
+
+        results = pd.DataFrame(grid_search.cv_results_)
+        results.sort_values(by='mean_test_score', ascending=False, inplace=True)
+        results.reset_index(drop=True, inplace=True)
+        print(results[['param_n_estimators','param_learning_rate','param_max_depth', 'mean_test_score', 'std_test_score']].head())
+
