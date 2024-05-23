2245829c750cf149dfe358e296075864fe5b91cb
FabioUMa2 <149825824+FabioUMa2@users.noreply.github.com>
2024-05-23 15:09:21 +0100

    Ajustar Modelo DBSCAN

--- Clustering.py
+++ Clustering.py
@@ -19,65 +19,39 @@ class Clustering:
 
     def dbscan(self):
 
-        # Lê o arquivo CSV contendo as avaliações de hotéis
-        df = pd.read_csv('Hotel_Reviews.csv')
+        # Supondo que self.data_loader já esteja definido e contém data_train, labels_train, data_test, labels_test
+        cols = ['Average_Score', 'Review_Total_Negative_Word_Counts', 'Review_Total_Positive_Word_Counts']
+        data_train = self.data_loader.data_train.loc[:, cols]
+      
 
-        # Remove linhas com valores ausentes (NA)
-        df.dropna(inplace=True)
-
-        # Seleciona as primeiras 5000 linhas e as colunas especificadas como conjunto de treino
-        X_train = df.loc[:4999,
-                  ['Average_Score', 'Review_Total_Negative_Word_Counts', 'Review_Total_Positive_Word_Counts']]
-
-        # Aplica o algoritmo de clustering DBSCAN ao conjunto de treino
-        clustering = DBSCAN(eps=10, min_samples=5).fit(X_train)
-
-
-        # Save the models to files using pickle
-        with open('Models/DBSCAN.pkl', 'wb') as dt_file:
-            pickle.dump(clustering, dt_file)
-
-
-        # Cria uma cópia do conjunto de treino e adiciona a coluna 'Cluster' com os rótulos do DBSCAN
-        DBSCAN_dataset = X_train.copy()
+        # Ajustar os parâmetros eps e min_samples
+        clustering = DBSCAN(eps=2, min_samples=5).fit(data_train)
+        DBSCAN_dataset = data_train.copy()
         DBSCAN_dataset['Cluster'] = clustering.labels_
 
-        # Identifica os outliers (pontos com rótulo -1)
         outliers = DBSCAN_dataset[DBSCAN_dataset['Cluster'] == -1]
 
-        # Extrai as características usadas para o clustering
-        features = X_train.columns
-        num_features = len(X_train.columns)
-
-        # Gera todas as combinações possíveis de duas características
+        features = data_train.columns
+        num_features = len(data_train.columns)
         combinations_list = list(combinations(features, 2))
 
-        # Cria uma grade de subplots para visualizar as combinações das características
         fig, axes = plt.subplots(num_features, num_features, figsize=(6 * num_features, 6 * num_features))
 
-        # Itera sobre todas as características para preencher a grade de subplots
         for i in range(num_features):
             for j in range(num_features):
                 if i != j:
-                    # Seleciona a combinação de características atual
                     feature_x, feature_y = features[i], features[j]
-                    # Remove combinações duplicadas
                     if (feature_y, feature_x) in combinations_list:
                         combinations_list.remove((feature_y, feature_x))
-                    # Plota os dados do clustering, colorindo por rótulo de cluster
                     sns.scatterplot(x=feature_x, y=feature_y,
                                     data=DBSCAN_dataset[DBSCAN_dataset['Cluster'] != -1],
                                     hue='Cluster', ax=axes[i, j], palette='Set2', legend='full', s=200)
-                    # Plota os outliers como pontos pretos
                     axes[i, j].scatter(outliers[feature_x], outliers[feature_y], s=10, label='outliers', c="k")
                     axes[i, j].legend()
                     axes[i, j].set_title(f'{feature_x} vs {feature_y}')
                     plt.setp(axes[i, j].get_legend().get_texts(), fontsize='12')
                 else:
-                    # Desativa o subplot para a diagonal principal
                     axes[i, j].axis('off')
 
-        # Ajusta o layout dos subplots para evitar sobreposição
         plt.tight_layout()
-        # Exibe o gráfico
         plt.show()
